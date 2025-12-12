4aaa46d239b76c55c95f1b7b17da771a3188d740
8dspaces <mickqi1985@outlook.com>
2025-12-12 22:52:05 +0800

    init change

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'export',
   
   // Change 'your-repo-name' to your actual repository name
-  basePath: process.env.NODE_ENV === 'production' ? '' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/dexin.de' : '',
   
   typescript: {
     ignoreBuildErrors: true,
